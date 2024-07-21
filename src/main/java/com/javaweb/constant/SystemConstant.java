d4f03d54feb5bb32cda576bd100516dfd45d4d8c
lyan148 <vietlinh1482004@gmail.com>
2024-07-22 01:51:55 +0700

    update

--- src/main/java/com/javaweb/constant/SystemConstant.java
+++ src/main/java/com/javaweb/constant/SystemConstant.java
@@ -8,7 +8,7 @@ public class SystemConstant {
     public static final String USER_ROLE = "ROLE_USER";
     public static final String MANAGER_ROLE = "ROLE_MANAGER";
     public static final String HOME = "/trang-chu";
-    public static final String ADMIN_HOME = "/admin/home";
+    public static final String ADMIN_HOME = "/admin/building-list";
     public static final String MODEL = "model";
     public static final String INSERT_SUCCESS = "insert_success";
     public static final String UPDATE_SUCCESS = "update_success";
